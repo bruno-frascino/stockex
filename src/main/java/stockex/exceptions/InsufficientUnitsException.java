96499312f46e3b7877ff20ee778b49160433e3c7
Bruno Frascino <bfrascino80@gmail.com>
2022-11-10 12:03:50 +1100

    chore: checked exception adjustment

--- src/main/java/stockex/exceptions/InsufficientUnitsException.java
+++ src/main/java/stockex/exceptions/InsufficientUnitsException.java
@@ -1,5 +1,14 @@
 package stockex.exceptions;
 
 public class InsufficientUnitsException extends Throwable {
+	
+	private static final long serialVersionUID = 1L;
 
+	public InsufficientUnitsException() {
+		super("Insufficient units available");
+	}
+
+	public InsufficientUnitsException(Throwable err) {
+		super(err);
+	}
 }
--- src/main/java/stockex/exceptions/InvalidCodeException.java
+++ src/main/java/stockex/exceptions/InvalidCodeException.java
@@ -2,4 +2,14 @@ package stockex.exceptions;
 
 public class InvalidCodeException extends Throwable {
 
+  private static final long serialVersionUID = 1L;
+
+  public InvalidCodeException() {
+		super("Invalid stock code");
+	}
+
+  public InvalidCodeException(Throwable err) {
+		super(err);
+	}
+
 }
